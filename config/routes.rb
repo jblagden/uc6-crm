4de4ee160ac8218596dba58efd02ca43ab6e0533
Justin Blagden <jbemails2@gmail.com>
2016-11-10 20:06:48 -0600

    Added the finder controller + actions and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'finder#index'
+
+  get 'alphabetized' => 'finder#alphabetized', as: 'alphabetized'
+
+  get 'missing_email' => 'finder#missing_email', as: 'missing_email'
+
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
